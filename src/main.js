30ddac5ce99503106d5a398062424ae39b38a277
LordFitoi <nahumsantanam@gmail.com>
2022-01-10 16:15:32 -0400

    Vue.js removed

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import Router from './router.js';
-import "tailwindcss/tailwind.css";
-import "./assets/style.css";
-
-createApp(App).use(Router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+import { resolve } from 'path' 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [
+        viteCompression({
+            filter: /\.(js|mjs|json|css)$/i
+        }),
+    ],
 })
