da2619cb491833cbc25fc31519caeb37455725fb
LordFitoi <nahumsantanam@gmail.com>
2022-01-11 09:03:28 -0400

    Now Leo's portfolio is a vanilla multi page website

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,19 @@ import viteCompression from 'vite-plugin-compression';
 export default defineConfig({
     plugins: [
         viteCompression({
+            deleteOriginFile: false,
             filter: /\.(js|mjs|json|css)$/i
         }),
     ],
+    build: {
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                kahkow: resolve(__dirname, "kahkow.html"),
+                cancercare: resolve(__dirname, "cancercare.html"),
+                alphaexpress: resolve(__dirname, "alpha-express.html"),
+            }
+        }
+    }
 })
