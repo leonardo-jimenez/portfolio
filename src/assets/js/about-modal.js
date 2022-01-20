5712cdee8d01ae3685f630228dab8c082ccfc83c
Louvens Raphael <hardow2011@outlook.com>
2022-01-20 14:05:55 -0400

    modal upgrades

--- src/assets/js/about-modal.js
+++ src/assets/js/about-modal.js
@@ -1,41 +1,40 @@
 const headerContainer = document.getElementById('header-container')
 const aboutButton = document.getElementById('header-about')
 const aboutModal = document.getElementById('about-modal')
-const aboutModalArrow = document.getElementById('about-modal-arrow')
 const aboutModalCloseButton = document.getElementById('about-modal-close-button')
+const modalBackground = document.getElementById('modal-background')
 
 aboutButton.addEventListener("click", function () {
     toggleModal()
-    setModalPosition()
 });
 
 aboutModalCloseButton.addEventListener("click", function () {
     toggleModal()
 });
 
+modalBackground.addEventListener("click", function () {
+    console.log('modalBackground')
+    toggleModal()
+});
+
+
 window.addEventListener('resize', function (event) {
-    setModalPosition()
 }, true);
 
 function toggleModal() {
-    console.log(aboutModal.style.display)
-    if (aboutModal.style.display != 'block') {
-        aboutModal.style.display = 'block'
+    console.log(modalBackground.style.display)
+    if (modalBackground.style.display != 'flex') {
+        modalBackground.style.display = 'flex'
+        document.body.style.overflow = 'hidden'
     } else {
-        aboutModal.style.display = 'none'
+        modalBackground.style.display = 'none'
+        document.body.style.overflow = 'auto'
     }
 }
 
-window.addEventListener('click', function(e){   
-    if (aboutModal.style.display == 'block' && !aboutModal.contains(e.target) && !aboutButton.contains(e.target)){
-        console.log('click')
-        toggleModal()
-    }
-  });
-
-function setModalPosition() {
-    aboutModal.style.top = headerContainer.offsetHeight - 7 + 'px'
-    let aboutButtonBounding = aboutButton.getBoundingClientRect()
-    let aboutModalBounding = aboutModal.getBoundingClientRect()
-    aboutModalArrow.style.left = aboutButtonBounding.left - aboutModalBounding.left + 'px'
-}
+// window.addEventListener('click', function(e){   
+//     if (modalBackground.style.display == 'block' && !modalBackground.contains(e.target) && !aboutButton.contains(e.target)){
+//         console.log('click')
+//         toggleModal()
+//     }
+//   });
