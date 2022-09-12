64d92d135b8c7cacee35fea42d344401051886dc
Louvens Raphael <hardow2011@outlook.com>
2022-09-12 14:51:58 -0400

    cancercare page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 module.exports = {
+    mode: 'jit',
     purge: [
-        './_site/**/*.html',
-        './_site/*.html'
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './_posts/*.md',
+        './*.html',
     ],
     theme: {
-        extend: {},
+        screens: {
+            'phone': '640px',
+            'tablet': '768px',
+            'laptop': '1024px',
+            'desktop': '1200px'
+        },
+        extend: {
+            colors: {
+                'kahkow-brand-color': '#202020',
+                'cancercare-brand-color': '#035A83',
+                'alpha-express-brand-color': '#084E8A',
+                'home-page-brand-color': '#1A2760',
+                'soft-blue': '#084E8A',
+                'color-tourcoise': '#035A83',
+                'smooth-gray': '#686B77',
+                'common-gray': '#858585',
+                'low-gray': '#707070',
+                'lite-gray': '#F2F2F2',
+            },
+        },
     },
-    variants: {},
     plugins: [],
 }
